4f3d2ec74d75fb9abd759ffb976569a669a52752
shodmanovv <150368733+shodmanovv@users.noreply.github.com>
2024-02-08 16:09:38 +0500

    adds

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,12 +86,28 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default    ',
+        'PASSWORD': 'c0jm1FaJEplP',
+        'HOST': 'ep-mute-water-a47rdrg1-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
